ebde8720cc29b9f217d3eef05792ae7f3f4499e8
shraykin <56413426+shraykin@users.noreply.github.com>
2019-11-28 21:50:20 -0600

    Add files via upload

--- Vending_Machine_Master.ino
+++ Vending_Machine_Master.ino
@@ -1,117 +1,200 @@
+/*
+
+
+*/
+
+
 #include <LiquidCrystal.h>
+#include <Stepper.h>
 #include <Key.h>
 #include <Keypad.h>
 
-LiquidCrystal lcd(1, 2, 4, 5, 6, 7); //pin order for LCD screen
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7); //pin order for LCD screen
 
-#define Password_length 5
+#define STEPS 2048 // the number of steps in one revolution for the stepper motor
+  
+Stepper stepperTL(STEPS, 10, 12, 11, 13);
+Stepper stepperTR(STEPS, 14, 16, 15, 17);
+Stepper stepperBL(STEPS, 18, 20, 19, 21);
+Stepper stepperBR(STEPS, 22, 24, 23, 25);
 
-char User_input[Password_length];
-char TL_Password[Password_length] = "1311";
-char TR_Password[Password_length] = "AD89";
-char BL_Password[Password_length] = "0000";
-char TR_Password[Password_length] = "3121";
-byte inputCounter;
-char pressedKey;
+const byte ROWS = 4; //four rows
 
+const byte COLS = 4; //three columns
 
-const byte ROWS = 4; 
-const byte COLS = 4; 
 char keys[ROWS][COLS] = 
 {
+
   {'1','2','3','A'},
-  {'4','5','6','B'},
+
+  {'4','5','6','B' },
+
   {'7','8','9','C'},
+
   {'*','0','#','D'}
+
 };
 
-byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
 
 Keypad matrixKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
+#define Password_length 5 /*password are 4 characters long but there is a null 
+                          character at the end */
 
-
-//top left corner motor
-#define TL_STEPPER_PIN_1 #
-#define TL_STEPPER_PIN_2 #
-#define TL_STEPPER_PIN_3 #
-#define TL_STEPPER_PIN_4 #
-
-//top right corner motor
-#define TR_STEPPER_PIN_1 #
-#define TR_STEPPER_PIN_2 #
-#define TR_STEPPER_PIN_3 #
-#define TR_STEPPER_PIN_4 #
-
-//bottom left corner motor
-#define BL_STEPPER_PIN_1 #
-#define BL_STEPPER_PIN_2 #
-#define BL_STEPPER_PIN_3 #
-#define BL_STEPPER_PIN_4 #
-
-//bottom right corner motor
-#define BR_STEPPER_PIN_1 #
-#define BR_STEPPER_PIN_2 #
-#define BR_STEPPER_PIN_3 #
-#define BR_STEPPER_PIN_4 #
-
+char User_input[Password_length]; //array for the users input on the keypad
+char TL_Password[Password_length] = "1311"; //top left password
+char TR_Password[Password_length] = "5432"; //top right password
+char BL_Password[Password_length] = "6542"; //bottom left password
+char BR_Password[Password_length] = "976A"; //bottom right password
+byte inputCounter; 
+char pressedKey;
+int forLoopCounter;
 
 void setup() 
+
 {
-  lcd.begin(16, 2);
+  lcd.begin(16, 2); //the dimensions for the LCD screen
   Serial.begin(9600);
 
-  pinMode(TL_STEPPER_PIN_1, OUTPUT);
-  pinMode(TL_STEPPER_PIN_2, OUTPUT);
-  pinMode(TL_STEPPER_PIN_3, OUTPUT);
-  pinMode(TL_STEPPER_PIN_4, OUTPUT);
-
-  pinMode(TR_STEPPER_PIN_1, OUTPUT);
-  pinMode(TR_STEPPER_PIN_2, OUTPUT);
-  pinMode(TR_STEPPER_PIN_3, OUTPUT);
-  pinMode(TR_STEPPER_PIN_4, OUTPUT);
-
-  pinMode(BL_STEPPER_PIN_1, OUTPUT);
-  pinMode(BL_STEPPER_PIN_2, OUTPUT);
-  pinMode(BL_STEPPER_PIN_3, OUTPUT);
-  pinMode(BL_STEPPER_PIN_4, OUTPUT);
-
-  pinMode(BR_STEPPER_PIN_1, OUTPUT);
-  pinMode(BR_STEPPER_PIN_2, OUTPUT);
-  pinMode(BR_STEPPER_PIN_3, OUTPUT);
-  pinMode(BR_STEPPER_PIN_4, OUTPUT);
-
-
+  lcd.setCursor(1,0); 
+  lcd.print("Hello welcome to");
+  lcd.setCursor(2,0);
+  lcd.print("Jeff's Machine");
+  delay(5000);
+  lcd.clear;
+  lcd.setCursor(1,3);
+  lcd.print("Please enter")
+  lcd.setCursor(2,3);
+  lcd.print("a password");
+  
+  
+  
+  
 }
 
 void loop() 
 {
-
-  lcd.setCursor(0,0);
-  lcd.print("Enter Password:");
-  
   pressedKey = matrixKeypad.getKey();
 
   if (pressedKey)
   {
-    User_input[inputCounter] = pressedKey;
-    lcd.setCursor(inputCounter, 1);
-    lcd.print(User_input[inputCounter]);
+    User_input[inputCounter] = pressedKey; //stores user input into the array
     Serial.println(User_input[inputCounter]);
-    inputCounter++;
+    lcd.clear;
+    lcd.setCursor(1,0);
+    lcd.print("You entered");
+    lcd.setCursor(2,0);
+    lcd.print(User_input);     //TEST THIS
+    inputCounter++;                        //moves to the next position in the array
   }
 
-  if (inputCounter == Password_length-1)
+  if (inputCounter == Password_length-1) //when the user presses four keys on the keypad
   {
-    lcd.clear();
-
     
+
+    if (!strcmp(User_input, TL_Password)) /*compares the user input array with the password
+                                          array for the top left*/
+    {
+      for (forLoopCounter = 0; forLoopCounter < 1; forLoopCounter++)
+      {
+       stepperTL.setSpeed(14);
+       stepperTL.step(2048);
+       Serial.println(forLoopCounter);
+      }
+      Serial.print("Hey this is inputCounter ");
+      Serial.println(inputCounter);
+      clearStorage();
+      Serial.print("Hey this is User_input[Password_length] ");
+      Serial.println(User_input[Password_length]);
+      Serial.print("Hey this is pressedKey ");
+      Serial.println(pressedKey);
+      Serial.print("Hey this is forLoopCounter ");
+      Serial.println(forLoopCounter);
+      Serial.print("Hey this is inputCounter after clear ");
+      Serial.println(inputCounter);
     }
+
+    if (!strcmp(User_input, TR_Password)) //compares the user input to top right
+    {
+      for (forLoopCounter = 0; forLoopCounter < 1; forLoopCounter++)
+      {
+       stepperTR.setSpeed(14);
+       stepperTR.step(2048);
+       Serial.println(forLoopCounter);
+      }
+      Serial.print("Hey this is inputCounter ");
+      Serial.println(inputCounter);
+      clearStorage();
+      Serial.print("Hey this is User_input[Password_length] ");
+      Serial.println(User_input[Password_length]);
+      Serial.print("Hey this is pressedKey ");
+      Serial.println(pressedKey);
+      Serial.print("Hey this is forLoopCounter ");
+      Serial.println(forLoopCounter);
+      Serial.print("Hey this is inputCounter after clear ");
+      Serial.println(inputCounter);
+    }
+
+    if (!strcmp(User_input, BL_Password)) //compares the user input to bottom left
+    {
+      for (forLoopCounter = 0; forLoopCounter < 1; forLoopCounter++)
+      {
+       stepperBL.setSpeed(14);
+       stepperBL.step(2048);
+       Serial.println(forLoopCounter);
+      }
+      Serial.print("Hey this is inputCounter ");
+      Serial.println(inputCounter);
+      clearStorage();
+      Serial.print("Hey this is User_input[Password_length] ");
+      Serial.println(User_input[Password_length]);
+      Serial.print("Hey this is pressedKey ");
+      Serial.println(pressedKey);
+      Serial.print("Hey this is forLoopCounter ");
+      Serial.println(forLoopCounter);
+      Serial.print("Hey this is inputCounter after clear ");
+      Serial.println(inputCounter);
+    }
+
+    if (!strcmp(User_input, BR_Password)) //compares the user input to bottom right
+    {
+      for (forLoopCounter = 0; forLoopCounter < 1; forLoopCounter++)
+      {
+       stepperBR.setSpeed(14);
+       stepperBR.step(2048);
+       Serial.println(forLoopCounter);
+      }
+      Serial.print("Hey this is inputCounter ");
+      Serial.println(inputCounter);
+      clearStorage();
+      Serial.print("Hey this is User_input[Password_length] ");
+      Serial.println(User_input[Password_length]);
+      Serial.print("Hey this is pressedKey ");
+      Serial.println(pressedKey);
+      Serial.print("Hey this is forLoopCounter ");
+      Serial.println(forLoopCounter);
+      Serial.print("Hey this is inputCounter after clear ");
+      Serial.println(inputCounter);
+    }
+    
+
+      
+ 
   }
-  
 }
 
-void TopLeft()
+void clearStorage()
 {
+  inputCounter = 0;
+  Serial.print("inputCounter = ");
+  Serial.println(inputCounter);
   
+  for(int eraseCounter = 0; eraseCounter <= inputCounter; eraseCounter++)
+  {
+    User_input[eraseCounter] = '\0'; /*goes to each address in the index and erases
+                                     the values*/
+  }
+}
